d496686b00b6d898b61ae15c9713012d466ada6a
Duilio Ponce Núñez <duilioponce2@gmail.com>
2025-04-25 15:59:23 -0600

    Add files via upload

---
+++ extra46.js
@@ -0,0 +1,36 @@
+function jecca()
+{
+    let email= document.getElementById("email").value;
+    let password= document.getElementById("password").value;
+
+    if (email==="hernandezmontesjessica5@gmail.com" && password=="010324")
+    {
+        window.location.href="extra46(2).html";
+    }
+    else
+    {
+        if (email!=="hernandezmontesjessica5@gmail.com" && password=="010324")
+        {
+            alert("Usuario no encontrado");
+        }
+        else
+        {
+            if (email==="hernandezmontesjessica5@gmail.com" && password!=="010324")
+            {
+                alert("Contraseña incorrecta");
+            }
+            else
+            {
+                if (email==="hernandezmontesjessica5@gmail.com" && password=="010324");
+                {
+                    alert("Usuario no encontrado");
+                }
+            }
+        }
+    }
+}
+
+function jeccaall()
+{
+    alert("¡Felicidades Jessica! Te amo muchísimo. Felices 12 🥳. Atte: Duilio Ponce Núñez; tu hermano mayor 💗.");
+}
